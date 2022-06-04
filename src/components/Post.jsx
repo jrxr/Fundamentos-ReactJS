54529b2cf5e753ee9aa4412462522a88a5951cc0
Agacy junior <juniormartinssom@gmail.com>
2022-06-04 15:53:46 -0300

    Inserindo keys

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -58,10 +58,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="">{line.content}</a>
               </p>
             );
@@ -85,8 +85,8 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        {comments.mpa((comment) => {
-          return <Comment content={comment} />;
+        {comments.map((comment) => {
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
