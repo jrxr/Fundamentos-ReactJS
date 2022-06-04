5ca295491a036936c578be5276fa71dd6a511672
Agacy junior <juniormartinssom@gmail.com>
2022-06-04 15:19:05 -0300

    Inserindo comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 
@@ -5,13 +6,11 @@ import styles from "./Post.module.css";
 
 import { Comment } from "./Comment";
 import { Avatar } from "./Avatar";
-import { useState } from "react";
 
 export function Post({ author, publishedAt, content }) {
-  const [comments, setComments] = useState([
-    1,
-    2,
-  ]);
+  const [comments, setComments] = useState(["Post muito bacana, hein!"]);
+
+  const [newCommentText, setNewCommentText] = useState("");
 
   const publishedDateFormatted = format(
     publishedAt,
@@ -29,8 +28,12 @@ export function Post({ author, publishedAt, content }) {
   function handleCreateNewComment() {
     event.preventDefault();
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
+  }
 
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
 
   return (
@@ -53,11 +56,15 @@ export function Post({ author, publishedAt, content }) {
       </header>
 
       <div className={styles.content}>
-        {content.map(line => {
-          if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
-          } else if (line.type === 'link') {
-            return <p><a href="">{line.content}</a></p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="">{line.content}</a>
+              </p>
+            );
           }
         })}
       </div>
@@ -65,7 +72,12 @@ export function Post({ author, publishedAt, content }) {
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder="Deixe um comentário..." />
+        <textarea
+          name="comment"
+          onChange={handleNewCommentChange}
+          value={newCommentText}
+          placeholder="Deixe um comentário..."
+        />
 
         <footer>
           <button type="submit">Publicar</button>
@@ -73,10 +85,8 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        {comments.mpa(comment => {
-          return (
-            <Comment />
-          )
+        {comments.mpa((comment) => {
+          return <Comment content={comment} />;
         })}
       </div>
     </article>
